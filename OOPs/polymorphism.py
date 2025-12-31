ab3f91673ad036f733bcd2b7713330285f581e8f
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-12-31 15:10:18 +0530

    update

--- OOPs/polymorphism.py
+++ OOPs/polymorphism.py
@@ -1,3 +1,6 @@
 # same thing ka alag lag jagah per name ya behave polymorphism 
 # do type ke hote hai - compile tiem/ run time
 # read in details about polymorphism.
+s = 'python'
+l = list(s)
+print(l)
---
+++ python.py
@@ -0,0 +1,10 @@
+# x = input('enter: ')
+# print(x)
+# print(type(x))
+# print(list(x))
+# print(x.split())
+
+x = eval(input())
+print(type(x))
+
+# commnd cosmazation
