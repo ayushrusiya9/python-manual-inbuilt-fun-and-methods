60b7eaf376735ce76496230d88fa5c615a8d35e5
Ayush Rusiya <ayushrusiya9@gmail.com>
2026-01-02 17:28:26 +0530

    update

--- list/common_elements.py
+++ list/common_elements.py
@@ -1,5 +1,16 @@
-def common_elements(l1,l2):
-    common_ele =[]
-    for n in l1:
-        pass
-        
+def common_elements(lst1, lst2):
+    common = []
+    for i in lst1:
+        for j in lst2:
+            if i == j:
+                already_added = False
+                for k in common:
+                    if k == i:
+                        already_added = True
+                if not already_added:
+                    common.append(i)
+    return common
+
+l = [1,2,1,1]
+l2 = [1,2,1,3]
+print(common_elements(l,l2))
